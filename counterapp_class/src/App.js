15801442f4f0381a1a99ce486dcb14d96991bbb7
vasu081992 <vasudevan29.92@gmail.com>
2024-05-12 13:05:39 +0530

    commit final

---
+++ counterapp_class/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterApp from './Components/Counter';
+function App() {
+  return (
+    <div className="App">
+<CounterApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ counterapp_class/src/Components/Counter.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class CounterApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
